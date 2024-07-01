6517ef8ce0be83484764e209ec733a2b787b2c5f
pierobramanti <piero.bramanti94@gmail.com>
2024-07-01 16:50:06 +0200

    [UPD] inserted console.log, snack 2 finished

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -4,9 +4,12 @@ let odd = [];
 // APRO CICLO FOR//
 for (i = 0; i < 6; i++) {
     let num = parseInt(prompt("Inserisci un numero"));
-}
+
 
 // IF //
 if (num % 2 !== 0) {
     odd.push(num);
+
 }
+// CONSOLE//
+console.log(odd)}
