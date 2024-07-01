058797094d543af8fab2eba8eb16af14e0e3ce19
pierobramanti <piero.bramanti94@gmail.com>
2024-07-01 17:41:29 +0200

    [UPD] declared variable "somma" and opened ciclo for

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -3,3 +3,9 @@ const userNumber = prompt("Inserisci un numero di 4 cifre")
 
 //Uso il metodo split("") salvando il risultante array in una variabile//
 let split = userNumber.split("");
+
+// Dichiaro una variabile per la somma, inizializzandola a 0 //
+let somma = 0;
+
+// Ciclo ogni cifra inserita fino alla lunghezza dell'array risultante dallo split() //
+for(let i=0; i < split.length; i++) {}
