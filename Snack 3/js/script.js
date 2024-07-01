4595ee21ea7a1a9cdf9fae668b0ee77b593d0c6f
pierobramanti <piero.bramanti94@gmail.com>
2024-07-01 17:35:54 +0200

    [UPD] inserted variable with split's method

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -1,2 +1,5 @@
 //Dichiaro una costante a cui assegno un valore con metodo prompt in cui chiedo all'utente di inserire 4 cifre//
 const userNumber = prompt("Inserisci un numero di 4 cifre")
+
+//Uso il metodo split("") salvando il risultante array in una variabile//
+let split = userNumber.split("");
