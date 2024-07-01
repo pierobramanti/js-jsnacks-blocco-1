f051968e775032b87d43f6a83515cb52bbbb77ff
pierobramanti <piero.bramanti94@gmail.com>
2024-07-01 15:47:53 +0200

    [UPD] inserted prompt

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,3 +1,7 @@
 // CREO UN ARRAY CONTENTENTE GLI INVITATI ALLA FESTA//
 
 let guest= ["Jay", "Daisy", "Tom", "Myrtle", "Nick", "Luigi", "Alessio", "Fabrizio"]
+
+// INSERISCO PROMPT//
+
+let nameGuest = prompt( "Gentile ospite, la preghiamo di fornire il suo nome").toLocaleLowerCase;
