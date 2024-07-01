d92b827166ddcfbae69df78cd745a536be7f4642
pierobramanti <piero.bramanti94@gmail.com>
2024-07-01 18:13:33 +0200

    [UPD] finished snack 3

--- Snack 3/README.md
+++ Snack 3/README.md
@@ -1,7 +1,9 @@
 1 Dichiaro una costante a cui assegno un valore con metodo prompt in cui chiedo all'utente di inserire 4 cifre
 2 Uso il metodo split("") salvando il risultante array in una variabile
-3 Dichiaro una variabile per la somma, inizializzandola a 0
-4 Ciclo ogni cifra inserita fino alla lunghezza dell'array risultante dallo split()
-5 Dichiaro una variabile per trasformare i singoli caratteri in numeri
-6 Ad ogni iterazione inserisco il valore di ogni numero alla somma
-7 Stampo il risultato sulla console
+3 SE la lunghezza dell'array è diversa da 4
+3.1 Stampo su console "Il numero inserito non è valido"
+3.2 ALTRIMENTI  Dichiaro una variabile per la somma, inizializzandola a 0
+4 Ciclo ogni cifra inserita fino alla lunghezza dell'array 
+4.1 Dichiaro una variabile per trasformare i caratteri in numeri
+4.2 ad ogni iterazione, aggiungo il valore di ogni numero alla somma
+5 Stampo il risultato sulla console
--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -4,8 +4,21 @@ const userNumber = prompt("Inserisci un numero di 4 cifre")
 //Uso il metodo split("") salvando il risultante array in una variabile//
 let split = userNumber.split("");
 
-// Dichiaro una variabile per la somma, inizializzandola a 0 //
-let somma = 0;
+if(split.length !== 4) {
+    console.log("Il numero inserito non è valido")
+} 
 
-// Ciclo ogni cifra inserita fino alla lunghezza dell'array risultante dallo split() //
-for(let i=0; i < split.length; i++) {}
+else {
+
+// Dichiaro una variabile per la somma, inizializzandola a 0
+    let somma = 0;
+// Ciclo ogni cifra inserita fino alla lunghezza dell'array risultante dallo split()
+    for(let i=0; i < split.length; i++) {
+// Dichiaro una variabile per trasformare i caratteri in numeri
+        let splitNumber = parseInt(split[i]);
+// Per ogni iterazione, aggiungo il valore di ogni numero alla somma
+        somma += splitNumber
+    }   
+//  Stampo il risultato sulla console 
+    console.log(`La somma totale dei numeri da te inserita è ${somma}`)
+}
